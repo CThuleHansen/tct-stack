df33999727cd247186add8e86b514efc8eb2ca2b
Casper Thule <casper.thule@eng.au.dk>
2018-07-04 21:56:53 +0200

    This fixes #1. ExceptT is used instead of in JSConverter

--- src/JSConverter.hs
+++ src/JSConverter.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 module JSConverter where
+import qualified Control.Monad.IO.Class     as CMIC
 import qualified Control.Monad.Trans.Except as MTE
 import qualified Control.Monad.Trans.Maybe  as MTM
 import qualified Data.Aeson                 as Aeson
@@ -12,30 +13,30 @@ import qualified System.Path.NameManip      as SPNM (absolute_path)
 data AppCoSimConfigs = AppCoSimConfigs { multiModel  :: String
                                        , coSimConfig :: String}
 
-loadJsonFile :: Prelude.FilePath -> IO (Either String Aeson.Object)
-loadJsonFile path = Aeson.eitherDecode <$> BS.readFile path
+loadJsonFile :: Prelude.FilePath -> MTE.ExceptT String IO Aeson.Object
+loadJsonFile path = MTE.ExceptT $ Aeson.eitherDecode <$> BS.readFile path
 
 -- traverse uses the IO Applicative Instance, which does not check for lefts/rights before it is done with all values in the object
 -- Switching to exceptT will make it use the exceptT Applicative Instance, which checks for lefts/rights on every value in the object.
 -- exceptT thereby does short-circuiting, which is desireable in this case.
-crFullPathFmus :: Aeson.Object -> IO (Either String Aeson.Object)
+crFullPathFmus :: Aeson.Object -> MTE.ExceptT String IO Aeson.Object
 crFullPathFmus obj = case HM.lookup "fmus" obj of
-  Nothing -> return . Left $ "key: \"fmus\" does not exist."
-  Just (Aeson.Object fmus) -> sequenceA <$> traverse createFullPath fmus
+  Nothing -> MTE.throwE $ "key: \"fmus\" does not exist."
+  Just (Aeson.Object fmus) -> traverse createFullPath fmus
     where
-        createFullPath :: Aeson.Value -> IO (Either String Aeson.Value)
+        createFullPath :: Aeson.Value -> MTE.ExceptT String IO Aeson.Value
         createFullPath (Aeson.String val) =
-          (Right . Aeson.String . T.pack) <$> SPNM.absolute_path ("resources/" ++ (T.unpack val))
-        createFullPath x = return (Left "Path not of type String")
+          (Aeson.String . T.pack) <$> CMIC.liftIO (SPNM.absolute_path ("resources/" ++ (T.unpack val)))
+        createFullPath x = MTE.throwE "Path not of type String"
 
-crMaestroMsgs :: AppCoSimConfigs -> IO (Either String Aeson.Object)
-crMaestroMsgs cfgs = MTE.runExceptT $ do
-  x <- MTE.ExceptT $ loadJsonFile (multiModel cfgs)
-  MTE.ExceptT $ crFullPathFmus x
+crMaestroMsgs :: AppCoSimConfigs -> MTE.ExceptT String IO Aeson.Object
+crMaestroMsgs cfgs = do
+  x <- loadJsonFile (multiModel cfgs)
+  crFullPathFmus x
 
 convertMessages :: AppCoSimConfigs -> IO()
 convertMessages cfgs = do
-  msgs <- crMaestroMsgs cfgs
+  msgs <- MTE.runExceptT $ crMaestroMsgs cfgs
   case msgs of
     Left err -> putStrLn $ "Failed with error: " ++ err
     Right x  -> putStrLn $ "Everything Succeeded " ++ (show x)
